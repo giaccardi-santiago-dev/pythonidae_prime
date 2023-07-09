66c762a9abaf65f8b05d84b4f0c5a7e5cef5aa19
Santiago <giaccardi.santiago@gmail.com>
2023-07-09 18:35:03 -0300

    baby steps

---
+++ guidelines.py
@@ -0,0 +1,42 @@
+"""
+Course guidelines
+
+Purpose
+Practical understanding of the Python language, thorought empirical acquariment of knowledge
+
+Methodology
+Usage of an code writing, code reading, code execution, code testing, code documentation,
+
+
+
+Tutorial section
+
+The usage of the triple quotation marks (\""" \""") -also known as DOCSTRING- is for the 
+purpose of making this file as interactive as possible, giving the reader the power to choose which
+part of the code to be executed, without over encumbering the terminal. The docstring usage will be
+given more detail once the special string literal category is reached far below.
+
+To use the striple quotation marks (\""" \""") properly these steps should be followed:
+1- Identify the given example you want to run inside this file 
+2- Add three " over the first comment below the example that says "# ~ Example finish"
+3- Add three " below the first comment over the example that says "# ~ Example start"
+4- Run the file and manipulate it as you see fit
+5- Remove the " you added by the step 2 and step 3 of this guide
+6- Go back to step 1
+
+The usage of the special character "#" -also known as COMMENT- is to tell interpreter to ignore the
+rest of the line when the
+program is run. It is used to provide information or explanations about the code for the readers.
+The usage of the extension "better comments" is advised, as it improves readability.
+
+The examples demand input of the reader and offers output for each example in order to give more
+interaction to the user with the learning process. The file asks for input by the implement of the
+function "input()" and offers output by the implementation of the function "print()", which will be
+covered in detail once the usage of functions has been reached.
+
+The steps to get the most out of this file is:
+1- Read the documentation about the desired subject, e.g: Variables.
+2- Choose the desired example to be tested, e.g: String data type.
+3- Experiment with the code, making changes and learning it's behavior by running the script
+4- Implement the knowledge into your own personal projects to solidify the learning process
+5- Advance logically to the next subject, e.g: Boolean data type.
